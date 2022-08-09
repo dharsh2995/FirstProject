4dbc596420c25dc4364e4b1bcb86b38b2359181f
dharsh2995 <priyabala2995@gmail.com>
2022-08-09 16:39:27 +0530

    firstcommit

---
+++ Array.java
@@ -0,0 +1,24 @@
+package array;
+
+public class Array {
+
+	public static void main (String[] args) {
+		
+		int a[] = new int [10];
+		
+		a[0]= 20;
+		a[1]= 100;
+		a[2]= 30;
+		a[3]= 40;
+		a[5]= 200;
+		
+		for (int i =0; i<a.length; i++) {
+		//int i = 0;
+		//	while(i<a.length) {
+				
+			System.out.println(a[i]);
+		//	i++;
+		}
+		
+	}
+}
